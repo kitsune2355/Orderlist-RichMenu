2729b6b3f9a08e4876f0b208b770f607aac3b4d7
unknown <61012819@rbac.ac.th>
2021-05-19 16:31:53 +0700

    make it better

---
+++ src/Order/OrderDetail.js
@@ -0,0 +1,47 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+
+const OrderDetail = () => {
+
+    const [loading, setLoading] = useState(true);
+    const [order, setOrder] = useState({});
+    const [product, setProduct] = useState({});
+
+    useEffect(() => {
+    axios.post(
+        'https://podsable.com:4017/order-online-bill',
+        {
+            od_online_number: 'OD-ARP17151227',
+            lang: 'th'
+        }
+        )
+    
+    .then((res)=>{
+        setLoading(false);
+        console.log(res.data);
+
+        if(res.data.isSuccess === true){
+            setOrder(res.data.data[0]);
+            setProduct(res.data.data[0].product[0]);
+        }
+    })
+    .catch(() => {
+        console.log('error')
+    });
+  }, []);
+
+  if (loading === true) {
+    return <div>loading...</div>
+  }
+  return <div >
+            <p>ประเภทการรับสินค้า : {order.receive_channel}</p>
+            <p>ราคาของออเดอร์ : {order.sum_price} บาท</p>
+            <p>ค่าส่ง : {order.delivery_cost} บาท</p>
+            <p>ส่วนลดค่าส่ง : {order.discount_delievry_cost} บาท</p>
+            <p>ช่องการการชำระ : {order.payment_channel_name}</p>
+            <p>ที่อยู่ที่จัดส่ง : {order.mem_address}</p>
+            <p>สินค้าทั้งหมด : {product.pr_name}</p>
+        </div>
+};
+
+export default OrderDetail
